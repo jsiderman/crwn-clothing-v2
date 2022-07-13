15ee628e0fe08a4f4cadaf9d137fcc30dfb8872f
jsiderman <jonsiderman@gmail.com>
2022-07-13 01:38:55 -0700

    cart item + context WIP

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,13 @@
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 
+import CartItem from '../cart-item/cart-item.component';
+
 const CartDropdown = () => {
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        <Button>Go To Checkout</Button>
-      </div>
+      <div className='cart-items'></div>
+      <Button>Go To Checkout</Button>
     </div>
   );
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from 'react';
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
 });
 
 export const CartProvider = ({ children }) => {
